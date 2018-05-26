4457dab0cb3b265fd3494560177c9c582a0ab837
olawale <olaclassik@gmail.com>
2018-05-26 07:55:41 +0100

    managing course module feature

---
+++ courses/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    url(r'^mine/$', views.ManageCourseListView.as_view(), name='manage_course_list'),
+
+    url(r'^create/$', views.CourseCreateView.as_view(), name='course_create'),
+
+    url(r'^(?P<pk>\d+)/module/$', views.CourseModuleUpdateView.as_view(), name='course_module_update'),
+
+    url(r'^(?P<pk>\d+)/edit/$', views.CourseUpdateView.as_view(), name='course_edit'),
+
+    url(r'^(?P<pk>\d+)/delete/$', views.CourseDeleteView.as_view(), name='course_delete'),
+]
--- courses/views.py
+++ courses/views.py
@@ -1,33 +1,78 @@
-from django.shortcuts import render
+from django.apps import apps
+from django.core.urlresolvers import reverse_lazy
+from django.forms.models import modelsform_factory
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic.base import TemplateResponseMixin, View
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
-from .models import Course
+
+from braces.views import LoginRequiredMixin, PermissionRequiredMixin
+
+from .forms import ModuleFormset
+from .models import Content, Course, Module
+
 # Create your views here.
 
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/formset.html'
+    course = None
 
-class ManageCourseListView(ListView):
+    def get_formset(self, data=None):
+        return ModuleFormset(instance=self.course,
+                            data=data)
+
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course,
+                                        id=pk,
+                                        owner=request.user)
+        return super(CourseModuleUpdateView, self).dispatch(request, pk)
+
+    def get(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset})
+
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset})
+        
+
+class OwnerMixin(object):
+    def get_queryset(self):
+        qs = super(OwnerMixin, self).get_queryset()
+        return qs.filter(owner=self.request.user)
+
+class OwnerEditMixin(object):
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super(OwnerEditMixin, self).form_valid(form)
+
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin):
     model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+    template_name = 'courses/manage/course/form.html'
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
     template_name = 'courses/manage/course/list.html'
 
-    def get_queryset(self):
-        queryset = super(ManageCourseListView, self).get_queryset()
-        return queryset.filter(owner=self.request.user)
+class CourseCreateView(PermissionRequiredMixin, OwnerCourseEditMixin, CreateView):
+    permission_required = 'courses.add_course'
 
+class CourseUpdateView(PermissionRequiredMixin, OwnerCourseEditMixin, UpdateView):
+    template_name = 'courses/manage/course/form.html'
+    permission_required = 'courses.change_course'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class CourseDeleteView(PermissionRequiredMixin, OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
+    success_url = reverse_lazy('manage_course_list')
+    permission_required = 'courses.delete_course'
