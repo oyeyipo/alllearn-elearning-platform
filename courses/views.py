5c689ab5f5df052470de83045b16ec8afac5c030
olawale <olaclassik@gmail.com>
2018-05-28 23:58:43 +0100

    display courses page done

--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,10 @@
 from django.apps import apps
 from django.core.urlresolvers import reverse_lazy
+from django.db.models import Count
 from django.forms.models import modelform_factory
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic.base import TemplateResponseMixin, View
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 
@@ -10,12 +12,30 @@ from braces.views import (CsrfExemptMixin, JsonRequestResponseMixin,
                           LoginRequiredMixin, PermissionRequiredMixin)
 
 from .forms import ModuleFormset
-from .models import Content, Course, Module
-from django.db.models import Count
-from .models import Subject
+from .models import Content, Course, Module, Subject
 
 # Create your views here.
 
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_modules=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects,
+                                        'subject': subject,
+                                        'courses': courses})
+
+    
 
 class ModuleOrderView(CsrfExemptMixin,
                         JsonRequestResponseMixin,
--- educa/urls.py
+++ educa/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
+from courses.views import CourseListView
+
 
 urlpatterns = [
     # Examples:
@@ -13,4 +15,5 @@ urlpatterns = [
     url(r'^accounts/logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^course/', include('courses.urls')),
+    url(r'^$', CourseListView.as_view(), name='course_list'),
 ]
